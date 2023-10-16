0408f2901b1b7f75f6deb88c1083700271a2d7c9
Kirill Smotrov <kirya.smotrov.02@mail.ru>
2023-10-16 12:34:45 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,45 @@
+import os
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.Qt import QDir
+class Widget(QtWidgets.QWidget):
+    def __init__(self, dir_path, parent=None):
+        super(Widget, self).__init__(parent)
+        le = QtWidgets.QLineEdit(textChanged=self.on_textChanged)
+
+        self._dirpath = dir_path
+
+        self.file_model = QtWidgets.QFileSystemModel()
+        self.file_model.setRootPath(self._dirpath)
+        self.file_model.setFilter(QDir.NoDotAndDotDot | QDir.AllDirs | QDir.Files | QDir.Hidden)
+        self.proxy_model = QtCore.QSortFilterProxyModel(
+            recursiveFilteringEnabled=True,
+            filterRole=QtWidgets.QFileSystemModel.FileNameRole)
+        self.proxy_model.setSourceModel(self.file_model)
+        
+        self.tree =  QtWidgets.QTreeView()
+        self.tree.setModel(self.proxy_model)
+        self.tree.setColumnWidth(500, 250)
+        self.set_root_index()
+
+        lay = QtWidgets.QVBoxLayout(self)
+        lay.addWidget(le)
+        lay.addWidget(self.tree)
+
+    @QtCore.pyqtSlot(str)
+    def on_textChanged(self, text):
+        self.proxy_model.setFilterWildcard("*{}*".format(text))
+        self.set_root_index()
+
+    def set_root_index(self):
+        root_index = self.file_model.index(self._dirpath)
+        proxy_index = self.proxy_model.mapFromSource(root_index)
+        self.tree.setRootIndex(proxy_index)
+
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    dirPath = os.path.expanduser('~')
+    print()
+    w = Widget(dirPath)
+    w.show()
+    sys.exit(app.exec_())
